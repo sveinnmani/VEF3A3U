7d5b343c30a2b84530c8605fe4e890cf7ec7a755
sveinnmani <sveinnmani@hotmail.com>
2017-09-05 22:05:21 +0000

    Add files via upload
    Set inn allar skrár sem notaðar voru í þessu verkefni.

---
+++ verk2_SMJ/application/view/_templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Min sida</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+
+    <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>css/utlit.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
+</head>
+<body>
+    <!-- logo, check the CSS file for more info how the logo "image" is shown -->
+    <!-- <div class="logo"></div> -->
+
+    <!-- navigation -->
+    <div class="navigation">
+        <a href="<?php echo URL; ?>">Steve Jobs</a>
+        <a href="<?php echo URL; ?>sveinn/biography">Biography</a>
+        <a href="<?php echo URL; ?>sveinn/pictures">Pictures</a>
+    </div>
+    <div class="bakgrunnur">
+        
+    </div>
---
+++ verk2_SMJ/application/view/heima/pictures.php
@@ -0,0 +1,14 @@
+<div class="galleryOver">
+	<div class="gallery1" width="600" height="400">
+		<!--<img src="" width="600" height="400">-->
+	</div>
+	<div class="gallery2" width="600" height="400">
+		<!--<img src="" width="600" height="400">-->
+	</div>
+	<div class="gallery3" width="600" height="400">
+		<!--<img src="" width="600" height="400">-->
+	</div>
+	<div class="gallery4" width="600" height="400">
+		<!--<img src="" width="600" height="400">-->
+	</div>
+</div>
